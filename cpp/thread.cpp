d741d4b5251c300cee70cb5f538f8653d6f6bcce
khle08 <khle08@qq.com>
2022-11-12 16:50:57 +0800

    add comment on thread.cpp

--- cpp/thread.cpp
+++ cpp/thread.cpp
@@ -4,25 +4,26 @@
 #include <iostream>
 #include <unistd.h>    // for "usleep"
 
-#include <chrono>
-using namespace std::chrono_literals;  // for "ms"
+// #include <chrono>
+// using namespace std::chrono_literals;  // for "ms"
+using namespace std::literals::chrono_literals;
 
 #define print(x) std::cout << x << std::endl
 
 
 void func1() {
-    for (int i = 0; i < 10; i++) 
-        print("+ ");
-
-    return;
+    for (int i = 0; i < 10; i++) {
+        print("f1::" << i << "\n");
+        std::this_thread::sleep_for(0.9s);
+    }
 }
 
 
 void func2(std::string p) {
-    for (int i = 0; i < 10; i++)
-        print(p);
-
-    return;
+    for (int i = 0; i < 10; i++) {
+        print(p << "::" << i << "\n");
+        std::this_thread::sleep_for(0.9s);
+    }
 }
 
 
@@ -33,7 +34,9 @@ void refresh(std::map<std::string, int> forecast)
             item.second++;
             print(item.first << " -> " << item.second);
         }
-        usleep(1 * 1000 * 1000);
+
+        // usleep(1 * 1000 * 1000);
+        std::this_thread::sleep_for(1s);
     }
     return;
 }
@@ -41,21 +44,24 @@ void refresh(std::map<std::string, int> forecast)
 
 int main()
 {
-    std::thread worker1(func1);
-    std::thread worker2(func2, "= ");
-    usleep(2 * 1000 * 1000);
-
     std::map<std::string, int> forecast = {
         {"Tokyo", 20},
         {"Taipei", 25},
         {"Berlin", 15}
     };
 
+    // Start running worker1 here ...
+    std::thread worker1(func1);
+    std::thread worker2(func2, " -> f2 ");
+    usleep(1 * 1000 * 1000);    // 1s
+
     std::thread bgWorker(refresh, forecast);
 
+    // ... but here worker1 and worker2 should wait for one loop execution of
+    // "bgWorker" and the next loop of worker1 and worker2 can keep running
+    bgWorker.join();
     worker1.join();
     worker2.join();
-    bgWorker.join();
 
     // Run the thread until the thread is "detach"ed.
     // worker1.detach();
