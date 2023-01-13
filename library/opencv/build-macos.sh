ecc26ef673f8c50d9aac8a4e5a839b6e50d7462f
khle08 <khle08@qq.com>
2023-01-14 00:56:25 +0800

    compile opencv

--- library/opencv/build-macos.sh
+++ library/opencv/build-macos.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "build opencv for android"
+# In order to 
+
+echo "build opencv for macOS"
 
 function build_opencv_modules
 {
@@ -8,10 +10,7 @@ function build_opencv_modules
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_CXX_FLAGS=-std=c++11 \
-    -DANDROID_STL=c++_shared \
     -DENABLE_CXX11=ON \
-    -DBUILD_ANDROID_PROJECTS=OFF \
-    -DBUILD_ANDROID_EXAMPLES=OFF \
     -DBUILD_JAVA=OFF  \
     -DBUILD_CUDA_STUBS=OFF \
     -DBUILD_DOCS=OFF \
@@ -30,16 +29,16 @@ function build_opencv_modules
     -DBUILD_WITH_DYNAMIC_IPP=OFF  \
     -DBUILD_ZLIB=OFF  \
     -DBUILD_opencv_apps=OFF \
-    -DBUILD_opencv_calib3d=OFF \
+    -DBUILD_opencv_calib3d=ON \
     -DBUILD_opencv_core=ON \
-    -DBUILD_opencv_features2d=OFF \
-    -DBUILD_opencv_flann=OFF \
+    -DBUILD_opencv_features2d=ON \
+    -DBUILD_opencv_flann=ON \
     -DBUILD_opencv_highgui=ON \
     -DBUILD_opencv_imgcodecs=ON \
     -DBUILD_opencv_imgproc=ON \
     -DBUILD_opencv_ml=OFF \
     -DBUILD_opencv_objdetect=OFF \
-    -DBUILD_opencv_photo=OFF \
+    -DBUILD_opencv_photo=ON \
     -DBUILD_opencv_python2=OFF \
     -DBUILD_opencv_python3=OFF \
     -DBUILD_opencv_shape=OFF \
@@ -48,8 +47,9 @@ function build_opencv_modules
     -DBUILD_opencv_ts=OFF \
     -DBUILD_opencv_video=ON \
     -DBUILD_opencv_videoio=ON \
-    -DBUILD_opencv_videostab=ON \
     -DBUILD_opencv_world=OFF \
+    -DBUILD_opencv_dnn=OFF \
+    -DWITH_CLP=ON \
     -DWITH_IPP=OFF \
     -DWITH_FFMPEG=OFF \
     -DWITH_CUDA=OFF \
@@ -63,10 +63,32 @@ function build_opencv_modules
     -DWITH_MATLAB=OFF \
     -DWITH_TBB=OFF \
     -DWITH_TIFF=OFF \
-    -DWITH_WEBP=OFF
+    -DWITH_WEBP=OFF \
+    -DOPENCV_EXTRA_MODULES_PATH=${EXTRA_MODULES} \
+    -DBUILD_opencv_videostab=ON \
+    -DBUILD_opencv_aruco=OFF \
+    -DBUILD_opencv_bgsegm=OFF \
+    -DBUILD_opencv_hfs=OFF \
+    -DBUILD_opencv_rapid=OFF \
+    -DBUILD_opencv_phase_unwrapping=OFF \
+    -DBUILD_opencv_intensity_transform=OFF \
+    -DBUILD_opencv_bioinspired=OFF \
+    -DBUILD_opencv_ccalib=OFF \
+    -DBUILD_opencv_line_descriptor=OFF \
+    -DBUILD_opencv_plot=OFF \
+    -DBUILD_opencv_reg=OFF \
+    -DBUILD_opencv_rgbd=OFF \
+    -DBUILD_opencv_xfeatures2d=ON \
+    -DBUILD_opencv_saliency=OFF \
+    -DBUILD_opencv_structured_light=OFF \
+    -DBUILD_opencv_surface_matching=OFF \
+    -DBUILD_opencv_optflow=ON \
+    -DBUILD_opencv_ximgproc=ON \
+    -DBUILD_opencv_xphoto=OFF \
+    # -DEIGEN_INCLUDE_PATH=${EIGEN} \
 
     make clean
-    make -j4
+    make -j6
     make install
 }
 
@@ -88,6 +110,7 @@ function build_opencv {
     build_opencv_modules
 }
 
-
+EXTRA_MODULES="/Users/kcl/Documents/Cpp_Projects/library/opencv/opencv_contrib-4.6.0/modules"
+EIGEN="/Users/kcl/Documents/Cpp_Projects/library/Eigen"
 build_opencv $(pwd)/mac
 
