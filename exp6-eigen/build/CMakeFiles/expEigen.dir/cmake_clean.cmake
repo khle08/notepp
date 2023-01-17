c6a836039abd3cd77d11667d1aaefdc27ab3e9b1
khle08 <khle08@qq.com>
2023-01-17 09:30:23 +0800

    add eigen examples

---
+++ exp6-eigen/build/CMakeFiles/expEigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/expEigen.dir/main.cpp.o"
+  "expEigen.pdb"
+  "expEigen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/expEigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exp6-eigen/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+#include "Eigen/Dense"
+
+#define print(x) std::cout << x << std::endl;
+
+
+int main(int argc, char const *argv[])
+{
+
+    print("\n===== Eigen/Dense testing [start] =====");
+    print("Eigen/Dense included !!!");
+    print(  "===== Eigen/Dense testing [ end ] =====\n");
+
+    return 0;
+}
