353f32362fa7ad749e4021bb8b93ba542a54aa32
khle08 <khle08@qq.com>
2023-05-21 14:19:02 +0800

    modify json demo

--- exp8-json/main.cpp
+++ exp8-json/main.cpp
@@ -15,15 +15,6 @@
 
 #define print(x) std::cout << x << std::endl;
 
-/** \brief Write the Value object to a stream.
- * Example Usage:
- * $g++ streamWrite.cpp -ljsoncpp -std=c++11 -o streamWrite
- * $./streamWrite
- * {
- *     "Age" : 20,
- *     "Name" : "robin"
- * }
- */
 
 int main() {
     Json::Value j1, j2, j3;
