5eaf9458fba2929316ca2c145d17b36c4f0a7c6f
khle08 <khle08@qq.com>
2023-11-16 20:17:45 +0800

    opencv rotate

--- exp/07-opencv/main.cpp
+++ exp/07-opencv/main.cpp
@@ -33,6 +33,30 @@ int main(int argc, char const *argv[])
         print("[SUCCESS] image size: " << img.size());
     }
 
+
+    print("\n===== OpenCV rotating [start] =====");
+
+    // cv::imshow("abc", img);
+    // cv::waitKey(0);
+
+    int angle = 90;
+    cv::Point2f center((img.cols - 1) / 2.0, (img.rows - 1) / 2.0);
+    cv::Mat rot = cv::getRotationMatrix2D(center, angle, 1.0);
+
+   // determine bounding rectangle, center not relevant
+    cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), img.size(), angle).boundingRect2f();
+    // adjust transformation matrix
+    rot.at<double>(0,2) += bbox.width / 2.0 - img.cols / 2.0;
+    rot.at<double>(1,2) += bbox.height / 2.0 - img.rows / 2.0;
+
+    // cv::Mat img;
+    cv::warpAffine(img, img, rot, bbox.size());
+
+    cv::imshow("abc", img);
+    cv::waitKey(0);
+
+    print("\n===== OpenCV rotating [ end ] =====");
+
     // cv::Rect_<double> rect1(0.3, 0.4, 3.11, 3.22);
     // print(rect1.x);           // 0.3
     // print(rect1.y);           // 0.4
@@ -56,38 +80,38 @@ int main(int argc, char const *argv[])
     // Object obj2{rect1, 1, 0.99};
 
     print("\n===== OpenCV thread [ start ] =====\n");
-    Cam cam = {-1, -1, img, false};
-    std::vector<Cam> imgVec = {cam};
+    // Cam cam = {-1, -1, img, false};
+    // std::vector<Cam> imgVec = {cam};
 
-    std::mutex imgMutex;
-    std::condition_variable cvMutex;
-    std::map<int, std::vector<Cam>> images = {};
+    // std::mutex imgMutex;
+    // std::condition_variable cvMutex;
+    // std::map<int, std::vector<Cam>> images = {};
 
-    std::vector<std::string> srcVec = {"/Users/kcl/Desktop/ADAS.mov", "1"};
-    for (int i = 0; i < srcVec.size(); i++) {
-        ImgReader imr(srcVec[i], i + 1, images, imgMutex);
-    }
-    usleep(3 * 1000 * 1000);    // 1s
+    // std::vector<std::string> srcVec = {"/Users/kcl/Desktop/ADAS.mov", "1"};
+    // for (int i = 0; i < srcVec.size(); i++) {
+    //     ImgReader imr(srcVec[i], i + 1, images, imgMutex);
+    // }
+    // usleep(3 * 1000 * 1000);    // 1s
 
-    std::map<int, std::vector<Cam>>::iterator it;
-    for (it = images.begin(); it != images.end(); it++) {
-        print("first: " << it->first << " || second: " << it->second.size());
-    }
+    // std::map<int, std::vector<Cam>>::iterator it;
+    // for (it = images.begin(); it != images.end(); it++) {
+    //     print("first: " << it->first << " || second: " << it->second.size());
+    // }
 
-    usleep(1 * 1000 * 1000);    // 1s
+    // usleep(1 * 1000 * 1000);    // 1s
 
-    std::map<int, std::vector<Cam>>::iterator it2;
-    for (it2 = images.begin(); it2 != images.end(); it2++) {
-        print("first: " << it2->first << " || second: " << it2->second.size());
-    }
+    // std::map<int, std::vector<Cam>>::iterator it2;
+    // for (it2 = images.begin(); it2 != images.end(); it2++) {
+    //     print("first: " << it2->first << " || second: " << it2->second.size());
+    // }
 
-    usleep(1 * 1000 * 1000);    // 1s
+    // usleep(1 * 1000 * 1000);    // 1s
 
-    std::map<int, std::vector<Cam>>::iterator it_debug;
-    for (it_debug = images.begin(); it_debug != images.end(); it_debug++) {
-        print("first: " << it_debug->first << " || second: " << it_debug->second.size());
-        print(it_debug->second[0].frame.size());
-    }
+    // std::map<int, std::vector<Cam>>::iterator it_debug;
+    // for (it_debug = images.begin(); it_debug != images.end(); it_debug++) {
+    //     print("first: " << it_debug->first << " || second: " << it_debug->second.size());
+    //     print(it_debug->second[0].frame.size());
+    // }
 
 
     // 3 ways to put element into map.
