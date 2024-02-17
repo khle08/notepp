6d9fd5829d11320079a9977f68773b1865088536
khle08 <khle08@qq.com>
2024-02-17 11:09:44 +0800

    add more details for convertion

--- library/mnn/build-ubuntu.sh
+++ library/mnn/build-ubuntu.sh
@@ -40,9 +40,23 @@ build_ubuntu
 echo "MNN compiled successfully !"
 
 
+# ======================================================================
+
 # [CONVERTION]
 # To convert onnx file into .mnn file, use the following CMD:
 #   $ ./MNNConvert -f ONNX --modelFile xxx/xxx/xxx.onnx --MNNModel xxx.mnn --bizCode MNN
+
+# Results:
+# The device support i8sdot:0, support fp16:0, support i8mm: 0
+# Start to Convert Other Model Format To MNN Model...
+# [10:19:28] :98: ONNX Model ir version: 7
+# [10:19:28] :99: ONNX Model opset version: 11
+# Start to Optimize the MNN Net...
+# inputTensors : [ images, ]
+# outputTensors: [ 339, 392, 445, output, ]
+# Converted Success!
+
+# ======================================================================
 
 # [QUANTIZATION]
 # To quantize model into int8 mode, use the following CMD:
@@ -68,3 +82,21 @@ echo "MNN compiled successfully !"
 #       "feature_quantize_method":"KL",
 #       "weight_quantize_method":"MAX_ABS"
 #   }
+
+# Results:
+# [10:58:51] @ 23: >>> modelFile: helmet5n.mnn
+# [10:58:51] @ 24: >>> preTreatConfig: quantize.json
+# [10:58:51] @ 25: >>> dstFile: helmet5n-int8.mnn
+# [10:58:51] @ 53: Calibrate the feature and quantize model...
+# [10:58:51] @ 156: Use feature quantization method: KL
+# [10:58:51] @ 157: Use weight quantization method: MAX_ABS
+# [10:58:51] @ 177: feature_clamp_value: 127
+# [10:58:51] @ 178: weight_clamp_value: 127
+# The device support i8sdot:0, support fp16:0, support i8mm: 0
+# [10:58:51] @ 111: used image num: 1000
+# [10:58:51] @ 668: fake quant weights done.
+# ComputeFeatureRange: 100.00 %
+# CollectFeatureDistribution: 100.00 %
+# [11:02:49] @ 58: Quantize model done!
+
+# ======================================================================
