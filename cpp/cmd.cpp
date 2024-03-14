9ad5bbd3fb73323983901f45d39f06dbf68961dc
khle08 <khle08@qq.com>
2024-03-14 23:40:08 +0800

    run cmd in c++ code

--- cpp/cmd.cpp
+++ cpp/cmd.cpp
@@ -1,8 +1,12 @@
 
+// link: https://stackoverflow.com/questions/31201631/execute-cmd-commands-using-c
+
 #include <iostream>
+// #include <windows.h>
 
 
 int main() {
-    std::system("echo abc");
+    std::system("echo abc");  // Linux / Mac
+    // WinExec("cmd", 1);     // Win
     return 0;
 }
