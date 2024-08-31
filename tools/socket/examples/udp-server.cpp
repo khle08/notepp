2868360312876edae7bfa9ed10c5da8eb8eded71
khle08 <khle08@qq.com>
2024-08-31 21:21:55 +0800

    modify udp

--- tools/socket/examples/udp-server.cpp
+++ tools/socket/examples/udp-server.cpp
@@ -16,8 +16,10 @@ using namespace std;
 
 int main()
 {
-    string ip;
-    uint16_t pt;
+    vector<string> ipVec;
+    vector<uint16_t> ptVec;
+    // string ip;
+    // uint16_t pt;
 
     // Initialize server socket..
     UDPServer<> udpServer;
@@ -34,8 +36,15 @@ int main()
     udpServer.onRawMessageReceived = [&](const char* message, int length, string ipv4, uint16_t port) {
         cout << ipv4 << ":" << port << " => " << message << "(" << length << ")" << endl;
 
-        ip = ipv4;
-        pt = port;
+        if (find(ipVec.begin(), ipVec.end(), ipv4) == ipVec.end()) {
+            ipVec.push_back(ipv4);
+        }
+        if (find(ptVec.begin(), ptVec.end(), port) == ptVec.end()) {
+            ptVec.push_back(port);
+        }
+        // ip = ipv4;
+        // pt = port;
+
         // Echo to client:
         udpServer.SendTo(message, length, ipv4, port);
     };
@@ -56,10 +65,12 @@ int main()
         BUFFER_INFO bzon = buzzer_on();
         BUFFER_INFO bzof = buzzer_off();
 
-        // udpServer.SendTo(input, ip, pt);
+        for (int i = 0; i < ipVec.size(); i++) {
+            udpServer.SendTo(input, ipVec[i], ptVec[i]);            
+        }
 
-        // Corner
-        udpServer.SendTo(b.arrchBuffer, b.nLen, "192.168.111.100", 8800);
+        // // Corner
+        // udpServer.SendTo(b.arrchBuffer, b.nLen, "192.168.110.64", 8800);
 
         // udpServer.SendTo(bzon.arrchBuffer, bzon.nLen, "192.168.111.62", 19999);
         // usleep(2.0 * 1000 * 1000);
