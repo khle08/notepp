10b2707e85dd1b3b145075f33206e84aa63839c1
khle08 <khle08@qq.com>
2025-01-24 13:51:08 +0800

    rm

--- exp/07-opencv-Hi3519DV500/toolchains/Hi3519DV500.cmake
+++
@@ -1,6 +0,0 @@
-
-SET(PLATFORM hi19dv500sdk)
-SET(CMAKE_C_COMPILER   aarch64-v01c01-linux-gnu-gcc)
-SET(CMAKE_CXX_COMPILER aarch64-v01c01-linux-gnu-g++)
-SET(CMAKE_STRIP        aarch64-v01c01-linux-gnu-strip)
-SET(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
